8228642ca38d55c3329a24d37d5bad5e582be264
Akash Deep <adeep@IT-C02SP37TG8WN.local>
2019-01-08 15:05:31 +0530

    fix

--- src/main/java/com/calculator/addservice/AddServiceApplication.java
+++ src/main/java/com/calculator/addservice/AddServiceApplication.java
@@ -2,6 +2,7 @@ package com.calculator.addservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
